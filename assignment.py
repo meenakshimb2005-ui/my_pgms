fd2f2e64b4709f419a11805e261dfec72471660a
meenakshicrab <meenakshimb2005@gmail.com>
2026-06-24 07:57:50 +0530

    fibanocci numbers upto 10

---
+++ assignment.py
@@ -0,0 +1,15 @@
+# Fibonacci series upto 10--HW
+#n=int(input("Enter the count--"))
+a=0
+b=1
+print(a)
+print(b)
+for i in range(2,11):
+    c=a+b
+    #if c<=10:
+    print(c)
+    a=b
+    b=c
+
+
+
